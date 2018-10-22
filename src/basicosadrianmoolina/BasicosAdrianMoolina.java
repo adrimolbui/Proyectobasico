bc5795ddc6b5880275698bf3a5d3c349ebdb1e17
Adrian Molina <adrimolbui@gmail.com>
2018-10-22 19:47:39 +0200

    metodo numericos

--- src/basicosadrianmoolina/BasicosAdrianMoolina.java
+++ src/basicosadrianmoolina/BasicosAdrianMoolina.java
@@ -10,12 +10,32 @@ package basicosadrianmoolina;
  * @author AdriPC
  */
 public class BasicosAdrianMoolina {
+    
 
+    
+    public static void numericos() {
+       //Estos son los tipos básicos
+        byte elMasPeque = 1;
+        short chiquitin = 1;
+        int entero=1;
+        long duplicaEntero = 11;
+        float grande = 200;
+        double doble=1.5;
+        
+        System.out.println("byte: "+ elMasPeque);
+        System.out.println("short: "+ chiquitin);
+        System.out.println("int: "+ entero);
+        System.out.println("long: "+ duplicaEntero);
+        System.out.println("float: "+ grande);
+        System.out.println("double: "+ doble);
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        numericos(); // Llamada al método
+        
+  
     }
-    
 }
